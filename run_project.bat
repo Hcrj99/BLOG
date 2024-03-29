2b38a21b03afacd58e81dd218185a4be30774bbf
hcrj99 <hcrj99@hotmail.com>
2024-03-29 17:45:58 -0500

    create bat to open all apps

---
+++ run_project.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\server\7.0\bin && mongod.exe -dbpath D:/data/db"
+start cmd.exe /k "cd C:\Users\USUARIO\Desktop\Desarrollo\FULL_STACK\APIS\API_REST_BLOG && npm start"
+start cmd.exe /k "cd C:\Users\USUARIO\Desktop\Desarrollo\FULL_STACK\PROJECTS\Blog && npm run dev"
+start C:\Users\USUARIO\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\USUARIO\AppData\Local\Postman\Postman.exe
