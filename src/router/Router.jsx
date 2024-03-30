c4e6d2c4b554a9e57ba25d5d7dde73f51f9859e7
hcrj99 <hcrj99@hotmail.com>
2024-03-30 18:14:50 -0500

    initial struct articles

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { Home } from "../Components/Pages/Home/Home";
-import { Articles } from "../Components/Pages/Articles";
+import { Articles } from "../Components/Pages/Articles/Articles";
 import { Header } from "../Components/Layout/Header/Header";
 import { Nav } from "../Components/Layout/Nav/Nav";
 import { Footer } from "../Components/Layout/Footer/Footer";
