ee19c94a176ab44fcef2e0f5bbba0279720861ab
hcrj99 <hcrj99@hotmail.com>
2024-04-01 19:25:07 -0500

    initial strcut create articles

--- src/Components/Pages/Create/CreateArticles.jsx
+++ src/Components/Pages/Create/CreateArticles.jsx
@@ -5,19 +5,19 @@ export const CreateArticles = () => {
     return (
         <div className='Create__container'>
             <h2>Create Article</h2>
-            <p>Form to create article</p>
-            <form className='Form__container'>
+            <p>Form through which you can create your own articles by adding image, title and the content that the article will cover.</p>
+            <form className='Form__container-create'>
                 <section>
                     <label htmlFor='file'>Image</label>
                     <input type='file' name='file'/>
                 </section>
                 <section>
                     <label htmlFor='title'>Title</label>
-                    <input type='text' name='title' placeholder='Title of the Articles' />
+                    <input type='text' name='title' placeholder='Title of the Article' />
                 </section>
                 <section>
                     <label htmlFor='content'>Content</label>
-                    <textarea type='text' name='Content' placeholder='Content of the article' />
+                    <textarea type='text' name='Content' placeholder='Content of the Article' />
                 </section>
                 <input type='submit' value='Save' />
             </form>
