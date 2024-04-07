4084bf03530f85cabfde29ef4cd282f7fdf2edec
hcrj99 <hcrj99@hotmail.com>
2024-04-06 20:37:02 -0500

    contact component

--- src/Components/Pages/Contact/Contact.jsx
+++ src/Components/Pages/Contact/Contact.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
 export const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='Contact__container'>
+            <h2>Contact me</h2>
+            <p>If you want to contact me or send me a message so I can read it, do not hesitate to write to me in the attached section or visit my social networks, I also leave you my email for more information, thank you very much for visiting my portfolio.</p>
+            <form className='Contact' action='mailto:hcrj99@hotmail.com'>
+                <input type='text' placeholder='Name'/>
+                <input type='text' placeholder='Last Name'/>
+                <input type='text' placeholder='E-mail'/>
+                <textarea type='text' placeholder='Message'/>
+                <input type='submit' value='Submit'/>
+            </form>
+        </div>
     )
 }
